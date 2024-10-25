4aed63afa0018a71bc993a104c68566e105947be
Sameer  Moyal <sameerab827@gmail.com>
2024-10-25 22:44:21 +0530

    add two int

---
+++ 2.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int AddTwpNum(int a,int b){
+    return a+b;
+}
+
+int main()
+{
+     int a,b;
+    cout<<"add two number : please enter two values : ";
+    cin>>a>>b;
+    cout<<"sum is : "<<AddTwpNum(a,b);
+
+    return 0;
+}
